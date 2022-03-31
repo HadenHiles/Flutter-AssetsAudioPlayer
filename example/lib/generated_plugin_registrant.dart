3cb4be96c6351955b794de5a5ba1e4a26cd53879
HadenHiles <hadenhiles@gmail.com>
2022-03-31 11:27:02 -0400

    Attempts to resolve duplicate exoplayer class dependency conflict.

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -6,11 +6,15 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:wakelock_web/wakelock_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   AssetsAudioPlayerWebPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
